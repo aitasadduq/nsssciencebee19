93e326cd92af738f739266e2defd822464628b0e
AIT <anas.tasadduq@gmail.com>
2019-11-27 03:30:51 +0500

    fetch news feed and timetable from Firebase

---
+++ lib/models/schedule.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class Schedule {
+  String name;
+  String date;
+  String startTime;
+  String endTime;
+
+  Schedule(this.name,this.date,this.startTime,this.endTime,);
+}
